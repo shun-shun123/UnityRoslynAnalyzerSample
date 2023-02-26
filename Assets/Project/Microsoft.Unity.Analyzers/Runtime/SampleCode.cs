09da2d14e28e412893f45bf5b2433d92ec58bed2
shun-shun123 <shunsuke15vb@gmail.com>
2023-02-27 00:13:14 +0900

    サンプル追加

--- Assets/Project/Microsoft.Unity.Analyzers/Runtime/SampleCode.cs
+++ Assets/Project/Microsoft.Unity.Analyzers/Runtime/SampleCode.cs
@@ -4,14 +4,17 @@ namespace Project.Microsoft.Unity.Analyzers.Runtime
 {
     public class SampleCode : MonoBehaviour
     {
+        [SerializeField]
+        private Transform target;
+        
         void Start()
         {
-        
         }
 
         void Update()
         {
             var foo = Time.fixedDeltaTime;
+            var message = target != null ? "not null" : "null";
         }
     }
 }
